4d99e9fc4cbd30a34eda6d710f663d221a336734
densuke5G <129590446+densuke5G@users.noreply.github.com>
2023-05-16 16:06:58 +0000

    asyncfav

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,9 +4,10 @@ class FavoritesController < ApplicationController
     favorite = Favorite.new(book_id: book.id)
     favorite.user_id = current_user.id
     # favorite.book_id = book.id
+    @book = Book.find(params[:book_id])
 
     favorite.save
-    redirect_back fallback_location: root_path
+    # redirect_back fallback_location: root_path
   end
 
   def destroy
@@ -14,8 +15,9 @@ class FavoritesController < ApplicationController
     favorite = Favorite.find_by(book_id: book.id)
     favorite.user_id = current_user.id
     # favorite.book_id = book.id
+    @book = Book.find(params[:book_id])
 
     favorite.destroy
-    redirect_back fallback_location: root_path
+    # redirect_back fallback_location: root_path
   end
 end
