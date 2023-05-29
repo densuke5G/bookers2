a81d3b2be213c9045bff32938a7fb1c38ae8a359
densuke5G <129590446+densuke5G@users.noreply.github.com>
2023-05-29 11:42:30 +0000

    commentasync

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,20 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.book_id = @book.id
 
-    comment.save
+    @book_comment.save
     flash[:notice] = 'Book comment was successfully created.'
-    redirect_to book_path(book)
+    # redirect_to book_path(book)
 
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book_comment = BookComment.find(params[:id]).destroy
+    @book = Book.find(params[:book_id])
+    # redirect_to book_path(params[:book_id])
 
   end
 
